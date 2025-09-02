9c0b3ac5445e0d42484c6ffe7dc423fd7c9e27dc
Arthur Hintz <114536155+arthurhintz@users.noreply.github.com>
2025-09-02 11:22:34 -0300

    Add files via upload

--- aula04/ex2.sql
+++ aula04/ex2.sql
@@ -4,12 +4,12 @@ CREATE EXTENSION postgis
 --os quais devem ser digitalizados no Google Earth para obtenção das coordenadas.
 --Crie uma trigger para calcular a área e o perímetro dos talhões a serem inseridos no banco de dados.
 
-CREATE TABLE area_experimental (cod int PRIMARY KEY,
+CREATE TABLE talhao (cod int PRIMARY KEY, cod_area integer,
 					geom geometry(polygon, 4326));
 
 
-alter table area_experimental add column area decimal(10,2);
-alter table area_experimental add column perimetro decimal(10,2);
+alter table talhao add column area decimal(10,2);
+alter table talhao add column perimetro decimal(10,2);
 
 create or replace function fun_area_perimetro()
 returns trigger as $$
@@ -22,15 +22,15 @@ end;
 $$
 language 'plpgsql';
 
-drop trigger if exists trg_area_perimetro on area_experimental;
+drop trigger if exists trg_area_perimetro on talhao;
 
 create trigger trg_area_perimetro before insert or update
-on area_experimental for each row execute procedure fun_area_perimetro();
+on talhao for each row execute procedure fun_area_perimetro();
 
 
 -- Inserindo dados
 
-INSERT INTO area_experimental (cod, geom) VALUES(1, ST_GeometryFromText('POLYGON
+INSERT INTO talhao (cod, cod_area,geom) VALUES(1, 1,ST_GeometryFromText('POLYGON
 (( -53.73368974181002 -29.71721494817317,
 		-53.73398320187583 -29.71898631813294,
 		-53.73359919220663 -29.71914284800592,
@@ -40,7 +40,7 @@ INSERT INTO area_experimental (cod, geom) VALUES(1, ST_GeometryFromText('POLYGON
 		-53.73368974181002 -29.71721494817317))',4326));
 
 
-INSERT INTO area_experimental (cod, geom) VALUES(2, ST_GeometryFromText('POLYGON
+INSERT INTO talhao (cod, cod_area, geom) VALUES(2,1, ST_GeometryFromText('POLYGON
 (( -53.73475985798689 -29.71752220003811,
 	-53.73526135146454 -29.71763978506916, 
 	-53.73531974872429 -29.71788825409705,
@@ -50,7 +50,7 @@ INSERT INTO area_experimental (cod, geom) VALUES(2, ST_GeometryFromText('POLYGON
 	-53.73462111707556 -29.71807040519297,
 	-53.73475985798689 -29.71752220003811))',4326));
 
-INSERT INTO area_experimental (cod, geom) VALUES(3, ST_GeometryFromText('POLYGON
+INSERT INTO talhao (cod,cod_area, geom) VALUES(3,1, ST_GeometryFromText('POLYGON
 (( -53.73550977916403 -29.71857791189412,
 	-53.73607993512331 -29.72035846241499,
 	-53.73534705789088 -29.72030851148511,
@@ -60,7 +60,7 @@ INSERT INTO area_experimental (cod, geom) VALUES(3, ST_GeometryFromText('POLYGON
 	-53.7346618740963 -29.71883048172944,
 	-53.73550977916403 -29.71857791189412))',4326));
 	
-INSERT INTO area_experimental (cod, geom) VALUES(4, ST_GeometryFromText('POLYGON
+INSERT INTO talhao (cod,cod_area, geom) VALUES(4,1, ST_GeometryFromText('POLYGON
 (( -53.73333362394837 -29.7196134197456,
 	-53.73221524139083 -29.72060712578091,
 	-53.73163286951691 -29.72177745605734,
@@ -74,7 +74,7 @@ INSERT INTO area_experimental (cod, geom) VALUES(4, ST_GeometryFromText('POLYGON
 	-53.73333362394837 -29.7196134197456))',4326));
 
 
-INSERT INTO area_experimental (cod, geom) VALUES(5, ST_GeometryFromText('POLYGON
+INSERT INTO talhao (cod,cod_area, geom) VALUES(5,1, ST_GeometryFromText('POLYGON
 (( -53.73433510839314 -29.71938941928779,
 	-53.73486019978724 -29.72083524548091,
 	-53.73475399988761 -29.72140753599267,
@@ -83,4 +83,4 @@ INSERT INTO area_experimental (cod, geom) VALUES(5, ST_GeometryFromText('POLYGON
 	-53.73303769737456 -29.72033311714463,
 	-53.73433510839314 -29.71938941928779))',4326));
 
-SELECT * from area_experimental
+SELECT * from talhao
