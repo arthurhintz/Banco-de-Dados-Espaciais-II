721ab2031b5a4192bfa3a65064ee5197be132bbe
Arthur Hintz <114536155+arthurhintz@users.noreply.github.com>
2025-09-16 10:24:59 -0300

    new trigger

--- aula06/aula06.sql
+++ aula06/aula06.sql
@@ -37,4 +37,49 @@ INSERT INTO construcao (nome, rua, numero, cod_bairro, geom) VALUES
 ('Facas Peão', 'Arroio Grande', 00, 25, ST_GeometryFromText('POINT
 (-53.66017133528313 -29.66880382989322)', 4326));
 
+-- TRIGGER AUDITORIA PARA SALVAR AS ALTERACOES DA TABELA
+
+
+CREATE TABLE auditoria_zona(
+	id SERIAL PRIMARY KEY,
+	nome character varying(50),
+	cod_bairro integer,
+	tipo_operacao VARCHAR(10),
+	data_operacao TIMESTAMP DEFAULT NOW(),
+	geom GEOMETRY(POINT, 4326)
+);
+
+
+CREATE OR REPLACE FUNCTION auditoria_zona_func()
+RETURNS TRIGGER AS $$
+BEGIN
+	IF	TG_OP = 'INSERT' THEN
+		INSERT INTO auditoria_zona(nome,cod_bairro, tipo_operacao, geom)
+		VALUES (NEW.nome, NEW.cod_bairro, 'INSERT', new.geom);
+	ELSIF	TG_OP = 'UPDATE' THEN
+		INSERT INTO auditoria_zona(nome, cod_bairro, tipo_operacao, geom)
+		VALUES (NEW.nome, NEW.cod_bairro, 'UPDATE', new.geom);
+	ELSIF	TG_OP = 'DELETE' THEN
+		INSERT INTO auditoria_zona(nome,cod_bairro, tipo_operacao, geom)
+		VALUES (OLD.nome, OLD.cod_bairro, 'DELETE', OLD.geom);
+	END IF;
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+CREATE TRIGGER tgr_auditoria_zona
+AFTER INSERT OR UPDATE OR DELETE ON construcao
+FOR EACH ROW
+EXECUTE FUNCTION auditoria_zona_func()
+
+
+
+INSERT INTO construcao(nome, rua, numero, cod_bairro, geom)
+VALUES ('Rótula Papelaria','Rod. RST-287', 7715,25, ST_GeomFromText('POINT(-53.7172552559166 -29.70541513216279)', 4326));
+
+
+SELECT * FROM auditoria_zona
+
+
+
+
 
